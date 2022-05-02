ce8814746b530bc69abc5c456eac947237de3f92
Tom-B-01 <barlowt5901@gmail.com>
2022-05-02 16:49:31 -0500

    Updated something2.rb to use fewer lines and trying out %w string-array literal notation

--- ruby_basics/something2.rb
+++ ruby_basics/something2.rb
@@ -1,3 +1,4 @@
+=begin
 loop do
   puts "Would you like me to print something? (y/n)"
   answer = gets.chomp.downcase
@@ -10,3 +11,13 @@ loop do
     puts "Invalid response. Please enter either 'y' or 'n'"
   end
 end
+=end
+
+loop do
+  puts "Do you want me to print something? (y/n)"
+  answer = gets.chomp.downcase
+  break if %w(y n).include?(answer) #This is one of the key things that I am refactoring. I could also do this with an OR operator
+  puts "Invalid input. Please enter either 'y' or 'n'"
+end
+
+puts "something" if answer == 'y'
