43e06e3aa396c060bb633bad7160b361b0b22904
Tom-B-01 <barlowt5901@gmail.com>
2022-10-14 10:53:21 -0500

    Added part of easy 5

--- small_problems/easy_5/after_midnight_1.rb
+++ small_problems/easy_5/after_midnight_1.rb
@@ -1,4 +1,13 @@
 def time_of_day (int)
   hours = (int / 60) % 24
   minutes = int % 60
-  str = "#{hours}:#{minutes}"
+  format('%02d:%02d', hours, minutes)
+end
+
+puts time_of_day(0)
+puts time_of_day(-3)
+puts time_of_day(35)
+puts time_of_day(-1437)
+puts time_of_day(3000)
+puts time_of_day(800)
+puts time_of_day(-4231)
